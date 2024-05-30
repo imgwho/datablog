1cce9675e61e96ec310eb031ea59efbe86a46afb
imgwho <imgwho@qq.com>
2024-05-30 23:31:13 +0800

    测试最简单的内容

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -21,7 +21,7 @@ export default defineConfig({
         },
         nav: [
             { text: '首页', link: '/' },
-            { text: '目录', link: '/pages/category' },
+            // { text: '目录', link: '/pages/category' },
             { text: '总览', link: '/pages/archives' },
             { text: '标签', link: '/pages/tags' },
             { text: '关于', link: '/pages/about' }
---
+++ posts/vitepress-first copy.md
@@ -0,0 +1,15 @@
+---
+date: 2022-06-30
+title: 测试
+category: 主题
+tags:
+- vitepress
+- markdown
+description: 测试
+---
+# 测试
+
+
+<Comment />
+
+
