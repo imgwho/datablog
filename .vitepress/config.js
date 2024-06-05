0ee87074924dad415985f9cdc1fa4d5744cd75dd
imgwho <imgwho@qq.com>
2024-06-05 15:01:07 +0800

    更新目录

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -6,6 +6,7 @@ const pageSize = 10
 
 export default defineConfig({
     lang: 'zh-Hans',
+    lastUpdated: true,
     title: '淡月清云的数据世界',
     base: '/',
     cacheDir: './node_modules/vitepress_cache',
